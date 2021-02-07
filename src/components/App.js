cdadc021a5291db1a50e4c4479f7913590aee85b
Amitabh  Baruah <amitabhyo@gmail.com>
2021-02-07 19:19:24 +0530

    final push

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,9 @@ export default class App extends Component {
                 <div  style={{ display: 'flex', justifyContent: 'center', position: 'relative'  }}>
                     <Board />
                 </div>
+                <div style={{paddingTop:'200px'}}> 
+                    <p style={{margin:'0'}}>Made with <span style={{color:'red',fontSize:'1.4em',paddingTop:'10'}}> ❤ </span> by blade365z</p> 
+                </div>
             </div>
         )
 
